70f3ab21af2c5e5c817790e4bda580a4dec70e3e
Kushagar Mahajan <42248680+Kushagar-Mahajan@users.noreply.github.com>
2020-03-30 04:36:48 +0530

    Boolean statement in Python

---
+++ booleans.py
@@ -0,0 +1,16 @@
+#basics about booleans and comparison operators
+
+a = 2 < 3 #check whether this is true of false
+print(a) #prints true or false of boolean a
+
+b = type(a) #check the class of a
+print(b)
+
+c = 2 == 3 #check whether it is true or false
+print(c)
+
+d = 2 != 3 #here != is not equal to
+print(d)
+
+e = 4 >= 3 #here >= is checked
+print(e)
