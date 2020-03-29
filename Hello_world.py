16149d44526d9ca041aa5d00b7ded3bea7588672
Kushagar Mahajan <42248680+Kushagar-Mahajan@users.noreply.github.com>
2020-03-29 16:25:31 +0530

    Add files via upload

---
+++ Hello_world.py
@@ -0,0 +1,21 @@
+#here we are going to learn about strings
+
+"hello" #method to write string is adding it in double quotes
+
+name = "Raj" #storing string in variable 
+print(type(name))
+
+#message = "John said to me "I will see you later"" will give you error
+message = 'John said to me "I will see you later"' #way to avoid string breaking
+print(message)
+
+#triple single or double quotes are used to print single paragraph with same concatination
+notice = '''If you are on a personal connection, 
+like at home, you can run an anti-virus scan on 
+your device to make sure it is not infected with malware.
+If you are at an office or shared network, 
+you can ask the network administrator to run 
+a scan across the network looking for misconfigured or 
+infected devices.'''
+print(notice)
+
